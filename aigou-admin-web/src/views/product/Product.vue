5e2cace07d740ec477c739c4fe4daa5ad81d2eb6
zhangruonan <zhangruonan@132.xom>
2019-10-20 08:48:55 +0800

    提交商品维护的前端代码

--- aigou-admin-web/src/views/product/Product.vue
+++ aigou-admin-web/src/views/product/Product.vue
@@ -12,6 +12,18 @@
 				<el-form-item>
 					<el-button type="primary" @click="handleAdd">新增</el-button>
 				</el-form-item>
+				<el-form-item>
+					<el-button type="primary" @click="handleViewProperties">显示属性</el-button>
+				</el-form-item>
+				<el-form-item>
+					<el-button type="primary" @click="handleSkuProperties">SKU属性</el-button>
+				</el-form-item>
+				<el-form-item>
+					<el-button type="primary" @click="handleOnSale">上架</el-button>
+				</el-form-item>
+				<el-form-item>
+					<el-button type="primary" @click="handleOffSale">下架</el-button>
+				</el-form-item>
 			</el-form>
 		</el-col>
 
@@ -41,7 +53,7 @@
 			</el-table-column>
 			<el-table-column label="操作" width="150">
 				<template scope="scope">
-					<el-button size="small" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+					<el-button size="small" @click="loadExtdate(scope.$index, scope.row),handleEdit(scope.$index, scope.row)">编辑</el-button>
 					<el-button type="danger" size="small" @click="handleDel(scope.$index, scope.row)">删除</el-button>
 				</template>
 			</el-table-column>
@@ -63,11 +75,35 @@
 				<el-form-item label="副标题" prop="subName">
 					<el-input v-model="editForm.subName"></el-input>
 				</el-form-item>
-				<div class="block" style="margin-left: 15px">
+
+				<el-form-item label="媒体属性" prop="medias">
+					<template>
+						<el-upload
+								class="upload-demo"
+								action="http://172.16.4.5:9527/services/common/file"
+								list-type="picture"
+								:on-success="handleSuccess"
+								:on-remove="handleRemove"
+								:file-list="fileList">
+							<el-button size="small" type="primary">点击上传</el-button>
+						</el-upload>
+					</template>
+				</el-form-item>
+
+				<el-form-item label="品牌">
+					<el-select v-model="editForm.brandId" clearable
+							   placeholder="请选择品牌">
+						<el-option
+								v-for="item in brands"
+								:label="item.name"
+								:value="item.id">
+						</el-option>
+					</el-select>
+				</el-form-item>
+				<div class="block" style="margin-left: 20px">
 					<span class="demonstration">商品类型</span>
 					<el-cascader
 							:show-all-levels="false"
-							@change="handleChange"
 							:change-on-select="true"
 							:props="defaultParams"
 							placeholder='请选择商品类型'
@@ -76,26 +112,14 @@
 							:clearable="true">
 					</el-cascader>
 				</div>
-				<el-form-item label="媒体属性" prop="logo">
-					<template>
-						<el-upload
-								class="upload-demo"
-								action="http://172.16.4.5:9527/services/common/file"
-								list-type="picture"
-								:on-remove="handleRemove"
-								:on-success="handleSuccess"
-								:before-upload="handleBeforeUpload"
-								:file-list="fileList"
-						>
-							<el-button size="small" type="primary">点击上传</el-button>
-						</el-upload>
-					</template>
-				</el-form-item>
-				<el-form-item label="商品描述">
-					<el-input type="textarea" v-model="editForm.description"></el-input>
+				<el-form-item label="商品描述" style="margin-top: 25px">
+					<el-input type="textarea" v-model="editForm.ext.description"></el-input>
 				</el-form-item>
 				<el-form-item label="商品详情">
-					<el-input type="textarea" v-model="editForm.richContent"></el-input>
+					<quill-editor
+							ref="QuillEditor"
+							v-model="editForm.ext.richContent"
+					></quill-editor>
 				</el-form-item>
 			</el-form>
 			<div slot="footer" class="dialog-footer">
@@ -103,6 +127,7 @@
 				<el-button type="primary" @click.native="editSubmit" :loading="editLoading">提交</el-button>
 			</div>
 		</el-dialog>
+
 
 		<!--新增界面-->
 		<el-dialog title="新增" v-model="addFormVisible" :close-on-click-modal="false">
@@ -147,7 +172,7 @@
 					</template>
 				</el-form-item>
 				<el-form-item label="商品描述">
-					<el-input type="textarea" v-model="addForm.ext.description" auto-complete="off"></el-input>
+					<el-input  type="textarea" v-model="addForm.ext.description" auto-complete="off"></el-input>
 				</el-form-item>
 				<el-form-item label="商品详情">
 					<quill-editor
@@ -159,6 +184,49 @@
 			<div slot="footer" class="dialog-footer">
 				<el-button @click.native="addFormVisible = false">取消</el-button>
 				<el-button type="primary" @click.native="addSubmit" :loading="addLoading">提交</el-button>
+			</div>
+		</el-dialog>
+
+		<!--显示属性维护-->
+		<el-dialog size="tiny" title="显示属性" v-model="viewPropertiesDialogVisible" :close-on-click-modal="false">
+			<el-form label-width="80px">
+				<el-form-item v-for="viewProperty in viewProperties" :label="viewProperty.specName">
+					<el-input v-model="viewProperty.value" auto-complete="off"></el-input>
+				</el-form-item>
+			</el-form>
+			<div slot="footer" class="dialog-footer">
+				<el-button @click.native="viewPropertiesDialogVisible = false">取消</el-button>
+				<el-button type="primary" @click.native="handleSaveViewProperties">提交</el-button>
+			</div>
+		</el-dialog>
+
+		<!--SKU属性维护-->
+		<el-dialog title="SKU属性" v-model="skuPropertiesDialogVisible" :close-on-click-modal="false">
+
+			<el-card class="box-card" v-for="(skuProperty,i) in skuProperties">
+				<div slot="header" class="clearfix">
+					<span style="line-height: 36px;">{{skuProperty.specName}}</span>
+				</div>
+				<div v-for="index in skuProperty.options.length+1" class="text item">
+					<el-row>
+						<el-col :span="18">
+							<el-input v-model="skuProperty.options[index-1]" auto-complete="off"></el-input>
+						</el-col>
+						<el-col :span="6">
+							<el-button @click="removeProperty(i,index-1)">删除</el-button>
+						</el-col>
+					</el-row>
+				</div>
+			</el-card>
+
+			<el-table :data="skus" highlight-current-row style="width: 100%;">
+				<el-table-column v-for="(value,key) in skus[0]" :label="key" :prop="key">
+				</el-table-column>
+			</el-table>
+
+			<div slot="footer" class="dialog-footer">
+				<el-button @click.native="skuPropertiesDialogVisible = false">取消</el-button>
+				<el-button type="primary" @click.native="handleSaveSkuProperties">提交</el-button>
 			</div>
 		</el-dialog>
 	</section>
@@ -175,9 +243,23 @@
 				filters: {
 					keyword: ''
 				},
+				skus:[],
+				//显示属性
+				viewProperties:[],
+				//显示sku属性
+				skuProperties:[],
+				//显示属性维护的模态框
+				viewPropertiesDialogVisible:false,
+				//显示sku属性的模态框
+				skuPropertiesDialogVisible:false,
+				//存放编辑页面新上传的图片
+				imgNewPath:[],
+				//存放修改图片时已有图片的保存位置
+				imgOriginalPath:[],
 				products: [],
 				options: [],
 				fileList:[],//文件上传  用作回显
+				productOptions:[],
 				brands:[],
 				defaultParams: {
 					label: 'name',
@@ -205,7 +287,6 @@
 					name: '',
 					subName: '',
 					productTypeId: null,
-					description:'',
 					brandId: null,
 					medias:'',
 					productTypes:[],
@@ -219,7 +300,10 @@
 				addLoading: false,
 				addFormRules: {
 					name: [
-						{ required: true, message: '请输入姓名', trigger: 'blur' }
+						{ required: true, message: '请输入标题', trigger: 'blur' }
+					],
+					subName: [
+						{ required: true, message: '请输入副标题', trigger: 'blur' }
 					]
 				},
 				//新增界面数据
@@ -232,13 +316,116 @@
 					productTypes:[],
 					ext:{
 						description:'',
-						richContent:''
+						richContent:'',
 					}
 				}
-
 			}
 		},
 		methods: {
+			//显示属性维护
+			handleViewProperties(){
+				//只能选中一行数据
+				if(this.sels.length==0){
+					this.$message({
+						message: '请选中一行数据',
+						type: 'warning'
+					});
+					return;
+				}
+				if(this.sels.length>1){
+					this.$message({
+						message: '只能选中一行数据',
+						type: 'warning'
+					});
+					return;
+				}
+				let productId = this.sels[0].id;
+				//查询要维护商品的显示属性
+				this.$http.get("/product/product/viewProperties/"+ productId)
+						.then(res=>{
+							this.viewProperties = res.data;
+						})
+				//打开模态框
+				this.viewPropertiesDialogVisible = true;
+			},
+
+			//显示属性保存
+			handleSaveViewProperties(){
+				let productId = this.sels[0].id;
+				this.$confirm("确认保存?",'提示',{
+					type:'warning'
+				}).then(() =>{
+					this.$http.post("/product/product/updateViewProperties?productId=" + productId,this.viewProperties)
+							.then(res=>{
+								let {success,message,restObj} = res.data;
+								if (success){
+									this.$message({
+										message:'保存成功',
+										type:'success'
+									});
+									this.viewPropertiesDialogVisible = false;
+								}else {
+									this.$message({
+										message:message,
+										type:'error'
+									});
+								}
+							})
+				}).catch(() =>{
+
+				});
+			},
+			//sku属性
+			handleSkuProperties(){
+				//只能选中一行数据
+				if(this.sels.length==0){
+					this.$message({
+						message: '请选中一行数据',
+						type: 'warning'
+					});
+					return;
+				}
+				if(this.sels.length>1){
+					this.$message({
+						message: '只能选中一行数据',
+						type: 'warning'
+					});
+					return;
+				}
+				let productId = this.sels[0].id;
+				//查询要维护商品的显示属性
+				this.$http.get("/product/product/skuProperties/"+ productId)
+						.then(res=>{
+							this.skuProperties = res.data;
+						})
+				//打开模态框
+				this.skuPropertiesDialogVisible = true;
+			},
+
+			//sku属性保存
+			handleSaveSkuProperties(){
+			},
+
+			//删除sku属性选项
+			removeProperty(index1,index2){
+				//index1  第几个sku属性   index2  属性d的第几个选项
+				this.skuProperties[index1].options.splice(index2,1);
+				//删除otions的索引为index2的元素
+
+			},
+
+			//上架
+			handleOnSale(){},
+			//下架
+			handleOffSale(){},
+
+			//获取富文本编辑框
+			loadExtdate(index,row){
+				this.$http.get("/product/productExt/findOne?productId="+row.id).then(res=>{
+					this.editForm.ext = res.data;
+				}).catch({});
+			},
+
 			//文件上传成功后的钩子函数
 			handleSuccess(response, file, fileList){
 				let {success,message,restObj} = response;
@@ -256,6 +443,22 @@
 					});
 				}
 				this.fileList = fileList;
+			},
+			//删除文件
+			handleRemove(file,fileList){
+				let fileId = '';
+				if (file.size){
+					fileId =file.response.restObj;
+				}else {
+					fileId = file.url.substring(20,file.url.length);
+				}
+				//删除服务器图片的同时，把我们保持原始数据的路径一并删除
+				this.imgOriginalPath.splice(this.imgOriginalPath.indexOf(FileId),1);
+				//删除图片的时候，把newImgPath数组里面的值向对应的删除
+				this.imgNewPath.splice(this.imgNewPath.indexOf(FileId),1);
+				this.$http.delete("/common/file?FileId="+FileId).then(res=>{
+
+				}).catch({})
 			},
 			//品牌
 			getBrands(){
@@ -368,12 +571,20 @@
 
 			//显示编辑界面
 			handleEdit: function (index, row) {
-				this.fileList = [];
+				this.fileList=[];
 				this.editFormVisible = true;
 				this.editForm = Object.assign({}, row);
-				this.fileList.push({"url":"http://172.16.4.164/" + row.medias})
-				this.loadGetPath(index, row)
+				this.loadGetPath(index, row);
+				this.fileList=[];
+				if(row.medias){
+					let arr =  row.medias.split(",");
+					for (var index = 0;index < arr.length;index++){
+						this.fileList.push({"url":"http://172.16.4.164"+arr[index]});
+						this.imgOriginalPath.push(arr[index]);
+					}
+				}
 			},
+
 			//显示新增界面
 			handleAdd: function () {
 				this.fileList = [];
@@ -381,16 +592,32 @@
 				this.addForm = {
 					name: '',
 					subName: '',
-					productTypeId: null,
-					brandId: null,
+					productTypeId: [],
+					brandId: '',
 					medias:'',
-					productTypes:[],
 					ext:{
 						description:'',
-						richContent:''
+						richContent:'',
 					}
 				};
 			},
+
+			/*编辑获取回填菜单*/
+			loadGetPath(index, row){
+				this.$http.get("/product/productType/"+row.productTypeId).then(res=>{
+					var selectedOptions = res.data.path.split('.');
+					let paths = [];
+					let indexs ='';
+					for (let index = 0 ; index < selectedOptions.length ; index++){
+						if (selectedOptions[index] != ''){
+							indexs = parseInt(selectedOptions[index]);
+							paths.push(indexs);
+						}
+					}
+					this.productOptions = paths;
+				}).catch({});
+			},
+
 			//编辑
 			editSubmit: function () {
 				this.$refs.editForm.validate((valid) => {
@@ -399,38 +626,37 @@
 							this.editLoading = true;
 							//NProgress.start();
 							let para = Object.assign({}, this.editForm);
-							para.birth = (!para.birth || para.birth == '') ? '' : util.formatDate.format(new Date(para.birth), 'yyyy-MM-dd');
-							editUser(para).then((res) => {
-								this.editLoading = false;
-								//NProgress.done();
-								this.$message({
-									message: '提交成功',
-									type: 'success'
-								});
-								this.$refs['editForm'].resetFields();
-								this.editFormVisible = false;
-								this.getProducts();
+							for(var i = 0;i<this.productOptions.length ; i++){
+								b = this.productOptions[i];
+							}
+							para.productTypeId = b;
+							//原有的数据已经用一个新数组接收了，现在需要把新添加的图片地址用了新的保存了
+							para.medias = this.imgNewPath.join(",");
+							this.$http.post("/product/product/add",para).then(
+									res=>{
+										let {success,message,restObj} = res.data;
+										if(success){
+											this.addLoading = false;
+											this.$message({
+												message: '编辑成功',
+												type: 'success'
+											});
+											this.$refs['editForm'].resetFields();
+											this.editFormVisible = false;
+											this.editLoading = false;
+											this.getBrands();
+										}else{
+											this.$message({
+												message: '编辑失败',
+												type: 'error'
+											});
+										}
+									}
+							).catch({
 							});
 						});
 					}
 				});
-			},
-
-			/*编辑获取回填菜单*/
-			loadGetPath(index, row){
-				this.$http.get("/product/productType/"+row.productTypeId).then(res=>{
-					var productOptions = res.data.path.split('.');
-					let paths = [];
-					let indexs ='';
-					for (let index = 0 ; index < productOptions.length ; index++){
-						if (productOptions[index] != ''){
-							indexs = parseInt(productOptions[index]);
-							paths.push(indexs);
-						}
-					}
-					console.debug(paths);
-					this.productOptions = paths;
-				}).catch({});
 			},
 
 			//新增
@@ -452,14 +678,14 @@
 										let {success,message,restObj} = res.data;
 										if(success){
 											this.addLoading = false;
-											//NProgress.done();
 											this.$message({
 												message: '添加成功',
 												type: 'success'
 											});
 											this.$refs['addForm'].resetFields();
 											this.addFormVisible = false;
-											this.getbrands();
+											this.fileList = [];
+											this.getProducts();
 										}else{
 											this.$message({
 												message: '添加失败',
@@ -468,7 +694,6 @@
 										}
 									}
 							).catch({
-
 							});
 						});
 					}
@@ -509,9 +734,35 @@
 			this.getProducts();
 			this.loadTypeTree();
 			this.getBrands();
+		},
+		//监听skuProperties
+		watch:{
+			skuProperties:{
+				handler(val,oldval){
+					//过滤掉options为空数组的sku属性
+					let skuPropertiesArr = this.skuProperties.filter(e=>e.options.length>0);
+					let result = skuPropertiesArr.reduce((pre,cur,currentIndex)=>{
+						let temp = [];
+						pre.forEach(e1=>{
+							cur.options.forEach((e2,index)=>{
+								let obj = Object.assign({},e1);
+								obj[cur.specName] = e2;
+								//判断是否是最后一次reduce
+								if (currentIndex == skuPropertiesArr.length-1){
+									obj.price = 0;
+									obj.store = 0;
+								}
+								temp.push(obj);
+							})
+						})
+						return temp;
+					},[{}])
+					this.skus = result;
+				},
+				deep:true
+			}
 		}
 	}
-
 </script>
 
 <style scoped>
